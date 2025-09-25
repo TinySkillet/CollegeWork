4518dbfdffaa47059681c8a769ac99dc1ec4aafa
Aditya <mirageaditya@gmail.com>
2025-09-25 11:18:14 +0545

    Add Aspect Oriented Programming Demo and Recycling Machine

--- Lab3 CIS/classwork/BadCar.java
+++ Lab3 CIS/classwork/BadCar.java
@@ -1,47 +1,19 @@
-// Not Encapsulated
-public class BadCar{
+// BAD CAR (Not Encapsulated)
+public class BadCar {
     public String color;
     public int speed;
     public String engineType;
 
-     public BadCar() {
-        // Empty constructor - fields will have default values (null, 0, null)
-        System.out.println("Default constructor called - creating a car with no specifications");
+    public void displayStatus() {
+        System.out.println("Car Status: " + color + ", " + speed + " km/h, " + engineType);
     }
 
-      // 2. Defined Constructor with parameters
-    public BadCar(String color, int speed, String engineType) {
-        this.color = color;
-        this.speed = speed;
-        this.engineType = engineType;
-        System.out.println("Parameterized constructor called - creating a " + color + " car");
+    public static void main(String[] args) {
+        BadCar car = new BadCar();
+        // Invalid usage - direct field access allows this
+        car.speed = -100; // Negative speed!
+        car.engineType = "Rocket"; // Invalid engine type!
+        car.color = null; // Null color!
+        car.displayStatus(); // Outputs nonsense
     }
-
-    public void drive() {
-        System.out.println(color + " car driving at " + speed + " km/h");
-    }
-
-    public void displayStatus(){
-        System.out.println("Car Status " + color + ", " + speed + " km/h, " + engineType);
-    }
-
-    public static void main(String[] args){
-        System.out.println("=== Using Default Constructor ===");
-        BadCar car1 = new BadCar(); // Using explicit default constructor
-        car1.color = "Blue";
-        car1.speed = 60;
-        car1.engineType = "Petrol";
-        car1.drive();
-        
-        System.out.println("\n=== Using Defined Constructor ===");
-        BadCar car2 = new BadCar("Red", 80, "Diesel"); // Using parameterized constructor
-        car2.drive();
-
-        System.out.println("\n=== Breaking the Car ===");
-        car2.speed = -100;
-        car2.engineType = "Rocket";
-        car2.color = null;
-        car2.displayStatus();
-    }
-    
 }
--- Lab3 CIS/classwork/GoodCar.java
+++ Lab3 CIS/classwork/GoodCar.java
@@ -1,29 +1,19 @@
-public class BadCar {
-    public String color;
-    public int speed;
-    public String engineType;
-    
-    // Default constructor is implicitly provided by Java
-    
-    public void drive() {
-        System.out.println(color + " car driving at " + speed + " km/h");
+// GOOD CAR (Encapsulated)
+class GoodCar {
+    private String color;
+    private int speed;
+
+    public void setSpeed(int s) {
+        if (s >= 0)
+            speed = s; // Validation!
     }
-    
-    public void displayStatus() {
-        System.out.println("Car Status: " + color + ", " + speed + " km/h, " + engineType);
+
+    public int getSpeed() {
+        return speed;
     }
-    
-    public static void main(String[] args) {
-        BadCar car = new BadCar(); // Using default constructor
-        
-        car.color = "Blue";
-        car.speed = 60;
-        car.engineType = "Petrol";
-        car.drive();
-        
-        car.speed = -100;
-        car.engineType = "Rocket";
-        car.color = null;
-        car.displayStatus();
+
+    public String getColor() {
+        return color;
     }
+    // ... other getters/setters ...
 }
